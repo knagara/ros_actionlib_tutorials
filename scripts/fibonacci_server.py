b7bb41a23dfb0cde413ee22420c4077b60544e0d
knagara <nagara.keita@gmail.com>
2019-03-28 15:36:16 +0900

    rename

--- scripts/fibonacci_server.py
+++ scripts/fibonacci_server.py
@@ -13,8 +13,8 @@ class FibonacciAction(object):
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start = False)
-        self._as.start()
+        self.server = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start = False)
+        self.server.start()
       
     def execute_cb(self, goal):
         # helper variables
@@ -32,21 +32,21 @@ class FibonacciAction(object):
         # start executing the action
         for i in range(1, goal.order):
             # check that preempt has not been requested by the client
-            if self._as.is_preempt_requested():
+            if self.server.is_preempt_requested():
                 rospy.loginfo('%s: Preempted' % self._action_name)
-                self._as.set_preempted()
+                self.server.set_preempted()
                 success = False
                 break
             self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
             # publish the feedback
-            self._as.publish_feedback(self._feedback)
+            self.server.publish_feedback(self._feedback)
             # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
             r.sleep()
           
         if success:
             self._result.sequence = self._feedback.sequence
             rospy.loginfo('%s: Succeeded' % self._action_name)
-            self._as.set_succeeded(self._result)
+            self.server.set_succeeded(self._result)
         
 if __name__ == '__main__':
     rospy.init_node('fibonacci')
